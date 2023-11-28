a48464d893c2013ecea8912bd18a02181115a3aa
Nikola Krezeski <nkrezeski@banzae.dev>
2023-11-28 22:47:21 +0100

    feat: implement actions per minute tracker
    - implement keyboard events detection in C
    - implement mouse events detection in C
    - calculate APM statistics
    - make the program run as a CLI with different arguments

---
+++ lib/detect-mouse-events.c
@@ -0,0 +1,65 @@
+#include <fcntl.h>
+#include <linux/input-event-codes.h>
+#include <linux/input.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+char *appendStringToChar(const char *destination, const char *source) {
+  // Calculate the total length of the combined string
+  size_t dest_len = strlen(destination);
+  size_t src_len = strlen(source);
+
+  // Allocate memory for the combined string
+  char *combined = (char *)malloc(dest_len + src_len + 1);
+
+  if (combined == NULL) {
+    printf("Memory allocation failed!");
+    return NULL;
+  }
+
+  // Copy the contents of the destination string to the combined string
+  strcpy(combined, destination);
+
+  // Copy the contents of the source string to the combined string
+  strcpy(combined + dest_len, source);
+
+  return combined;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s <device number>\n", argv[0]);
+    return 1;
+  }
+
+  char *device_file_path = "/dev/input/event";
+  const char *device_file = appendStringToChar(device_file_path, argv[1]);
+
+  struct input_event ev;
+  int fd, click_count = 0;
+  time_t start_time = time(NULL);
+  time_t current_time;
+
+  fd = open(device_file, O_RDONLY);
+  if (fd == -1) {
+    perror("Cannot open mouse device");
+    return 1;
+  }
+
+  while (1) {
+    read(fd, &ev, sizeof(struct input_event));
+
+    if (ev.type == EV_KEY && (ev.code == BTN_LEFT || ev.code == BTN_RIGHT) &&
+        ev.value == 1) {
+      click_count++;
+      printf("Click Count: %d\n", click_count);
+      fflush(stdout);
+    }
+  }
+
+  close(fd);
+  return 0;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,100 @@
-console.log('Hello World!')
+#!/usr/bin/env node
+
+import { spawn } from 'child_process'
+import { join } from 'path'
+
+interface IAction {
+  content: string
+  timestamp: number
+}
+
+const registerProcesses = (mouseFileId: string, keyboardFileId: string) => {
+  const mouseEventProcess = spawn(
+    join(__dirname, '../', 'lib', './detect-mouse-events'),
+    [mouseFileId],
+    { stdio: 'pipe' },
+  )
+
+  const keyboardEventsProcess = spawn(
+    join(__dirname, '../', 'lib', './detect-keyboard-events'),
+    [keyboardFileId],
+    { stdio: 'pipe' },
+  )
+
+  return { mouseEventProcess, keyboardEventsProcess }
+}
+
+const calculateAverageActionsPerMinute = (
+  mouseFileId: string,
+  keyboardFileId: string,
+  isVerbatim: boolean,
+) => {
+  let rawActions: IAction[] = []
+  const actions: IAction[] = []
+  let elapsedIntervals = 0
+
+  const { keyboardEventsProcess, mouseEventProcess } = registerProcesses(
+    mouseFileId,
+    keyboardFileId,
+  )
+
+  const addEvent = (event: string) => {
+    rawActions.push({
+      content: event,
+      timestamp: Date.now(),
+    })
+  }
+
+  const generateStatistics = () => {
+    const totalAverageActionsPerMinute = actions.length / elapsedIntervals
+
+    const actionsForLastMinute = actions.filter(
+      (action) => action.timestamp > Date.now() - 60000,
+    )?.length
+
+    const averageActionsPerMinuteForLastTenMinutes =
+      actions.filter((action) => action.timestamp > Date.now() - 10 * 60000)
+        ?.length / 10
+    if (isVerbatim) {
+      console.log(
+        `Actions for last minute: ${actionsForLastMinute} | Total average actions per minute: ${totalAverageActionsPerMinute} | Average actions per minute for last 10 minutes: ${averageActionsPerMinuteForLastTenMinutes}`,
+      )
+    } else {
+      console.log(`APM: ${actionsForLastMinute} `)
+    }
+  }
+
+  mouseEventProcess.stdout.on('data', (data) => {
+    addEvent(data.toString().trim())
+  })
+
+  keyboardEventsProcess.stdout.on('data', (data) => {
+    addEvent(data.toString().trim())
+  })
+
+  mouseEventProcess.on('close', (code) => {
+    console.log(`Mouse child process exited with code ${code}`)
+    process.exit(0)
+  })
+
+  keyboardEventsProcess.on('close', (code) => {
+    console.log(`Keyboard child process exited with code ${code}`)
+    process.exit(0)
+  })
+
+  generateStatistics()
+  setInterval(() => {
+    actions.push(...rawActions)
+    elapsedIntervals++
+    generateStatistics()
+    rawActions = []
+  }, 60000)
+}
+
+const [mouseFileId, keyboardFileId, isVerbatim] = process.argv.slice(2)
+
+if (!mouseFileId || !keyboardFileId) {
+  throw new Error('Please provide mouse and keyboard file ids')
+}
+
+calculateAverageActionsPerMinute(mouseFileId, keyboardFileId, !!isVerbatim)
